54dff458c0b23f15dedd0c9de601ab507d9d638d
Akhil Kizhakkoote Mathews <s2266637@ed.ac.uk>
2023-05-17 08:44:29 +0100

    updated

--- tempconv.py
+++ tempconv.py
@@ -1,2 +1,19 @@
-def temperature_conversion(celcius)
+def temperature_conversion(celcius):
+     """
+     Converts temperature in celcius to fahrenheit
+     """
      return celcius + 273.15
+
+def mean_temperature(data):
+     """
+     Get mean temperature
+
+     Args:
+           data (pandas.DataFrame): data with air temperature measurements.
+
+     Returns:
+           float: mean temperature
+
+     """
+     temperatures=data['Air temperature (degC)']
+     return sum(temperatures)/len(temperatures)
